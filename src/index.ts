0d462e27b6e4dcb5231a9f6ac80706bf1dd2d66d
Davi Koscianski Vidal <davi.koscianski-vidal@sonarsource.com>
2023-07-28 16:57:02 +0200

    whatever

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { app } from "./server.js";
 import { port } from "./config.js";
 
+try {
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
+} catch (err) {
+  throw err;
+}
+
+
+try {
+  
+} catch (error) {
+  throw error;
+  
+}
